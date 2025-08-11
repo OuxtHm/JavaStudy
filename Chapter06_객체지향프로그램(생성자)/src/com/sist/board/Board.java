0396e5e5afd716a8a21f589fedb053c1b5f9f238
sist <sist@sist39>
2025-08-11 17:49:22 +0900

    day15

---
+++ Chapter06_객체지향프로그램(생성자)/src/com/sist/board/Board.java
@@ -0,0 +1,10 @@
+package com.sist.board;
+
+public class Board {
+
+	void sayHello2()
+	{
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ Chapter06_객체지향프로그램(생성자)/src/com/sist/board/Main1.java
@@ -0,0 +1,11 @@
+package com.sist.board;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Board b = new Board();
+		b.sayHello2();
+
+	}
+
+}
