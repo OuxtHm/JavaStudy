c8625df3334afd44819b0f233cce281ceaa444a1
sist <sist@sist39>
2025-08-13 17:37:37 +0900

    day17

---
+++ Chapter07_객체지향프로그램(재사용)/src/com/sist/main/재사용_2.java
@@ -0,0 +1,25 @@
+package com.sist.main;
+
+/*
+	class A
+	{
+		int a;
+		int b;
+		public void aaa(){}
+	}
+	class B extends A
+	{
+		int c;
+		public void bbb(){}
+	}	
+
+	B bb = new B();
+*/
+public class 재사용_2 {
+
+	public static void main(String[] args) {		
+		
+
+	}
+
+}
