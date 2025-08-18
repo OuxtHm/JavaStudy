183277c67c67dcb9d38973734a935706e774b237
sist <sist@sist39>
2025-08-18 17:50:55 +0900

    day18

---
+++ Chpater08_클래스종류/src/com/sist/ast/GineMusic.java
@@ -0,0 +1,10 @@
+package com.sist.ast;
+
+public class GineMusic extends MusicClass{
+	// if -> 어노테이션 -> index역할
+	@Override
+	public void musicAllData() {
+		System.out.println("GineMusic 크롤링 완료");
+		
+	}
+}
---
+++ Chpater08_클래스종류/src/com/sist/ast/Melon.java
@@ -0,0 +1,13 @@
+package com.sist.ast;
+
+public class Melon extends MusicClass{
+
+	@Override
+	public void musicAllData() {
+		System.out.println("Melon 크롤링 완료");
+		
+	}
+
+	
+
+}
